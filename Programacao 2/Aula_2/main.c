267c50140c169f3f54e14a33cb79bdcacc227d8a
henrique2020 <henriqueb.hahn2000@gmail.com>
2022-03-14 21:07:07 -0300

    aula_2

--- Programacao 2/Aula_2/main.c
+++ Programacao 2/Aula_2/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-#define N 5
 int main(void){
-    int mat[N][N];
+    int tam;
+    printf("Digite o tamanho da matrix: ");
+    scanf("%d", &tam);
+    printf("\n");
+    int mat[tam][tam];
     int i, j;
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            if(i > 0 && i%2 != 0){
-                scanf("%d", &mat[N-i][j]);
+    for (int i = 0; i < tam; i++){
+        for (int j = 0; j < tam; j++){
+            if((i > 0 && i%2 != 0) && tam%2 != 0){
+                scanf("%d", &mat[(tam-1)-i][j]);
             }
             else{
                 scanf("%d", &mat[i][j]);
@@ -17,8 +20,8 @@ int main(void){
     
     printf("\n");
 
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
+    for(i = 0; i < tam; i++){
+        for(j = 0; j < tam; j++){
             printf("%d ", mat[i][j]);
         }           
       printf("\n");
