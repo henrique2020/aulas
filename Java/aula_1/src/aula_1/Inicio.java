a0a127814959435b8c39f07159fd15941696c591
Henrique Hahn <henriqueb.hahn2000@gmail.com>
2022-08-08 20:53:55 -0300

    aula_1 e atv_1

---
+++ Java/aula_1/src/aula_1/Inicio.java
@@ -0,0 +1,8 @@
+package aula_1;
+
+public class Inicio {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ ex1/Primo.java
@@ -0,0 +1,42 @@
+package ex1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Primo {
+    public boolean Verificador(int num) {
+        if(num < 2){
+            return false;
+        }
+        else if (num == 2) {
+            return true;
+        } else {
+            for (int i = 2; i < (int) num+1/2; i++) {
+                if(num%i == 0){
+                    return false;
+                }
+            }
+            return true;  
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Primo primo = new Primo();
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+        
+        System.out.print("Digite o número que deseja verificar: ");
+        int num = Integer.parseInt(in.readLine());
+        
+
+        if(primo.Verificador(num)){
+            System.out.printf("%d é primo", num);
+        }
+        else{
+            System.out.printf("%d não é primo", num);
+        }
+
+        in.close();
+        ir.close();
+    }
+}
